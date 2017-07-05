a5a3bbbeb57e2f5a854b661d8f8a90a4e888e41a
andrasat <andra.satria1@gmail.com>
2017-07-05 16:32:03 +0700

    start using echo framework

--- main.go
+++ main.go
@@ -1,56 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "encoding/json"
-  "github.com/gorilla/mux"
+  "github.com/labstack/echo"
+  "github.com/labstack/echo/engine/standard"
+  "github.com/labstack/echo/middleware"
 )
 
-type Article struct {
-  ID int `json:"id"`
-  Title string `json:"title"`
-  Desc string `json:"desc"`
-  Content string `json:"content"`
-}
-
-type Articles []Article
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "This is a Homepage")
-  fmt.Println("Endpoint: Homepage")
-  fmt.Println("-----")
-}
-
-func getAllArticles(w http.ResponseWriter, r *http.Request) {
-  articles := Articles{
-    Article{Title: "test", Desc: "test", Content: "test"},
-    Article{Title: "test", Desc: "test", Content: "test"},
-  }
-  fmt.Println("Endpoint: getAllArticles")
-
-  json.NewEncoder(w).Encode(articles)
-}
-
-func getOneArticle(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  key := vars["id"]
-
-  fmt.Fprintf(w, "Key: "+key)
-}
-
-func handleRequest() {
-  myRouter := mux.NewRouter().StrictSlash(true)
-
-  myRouter.HandleFunc("/", homePage)
-  myRouter.HandleFunc("/all", getAllArticles)
-  myRouter.HandleFunc("/article/{id}", getOneArticle)
-
-  log.Fatal(http.ListenAndServe(":3000", myRouter))
-}
-
 func main() {
-  fmt.Println("Rest API - Mux Router")
-  handleRequest()
+  // Echo instance ?
+  e := echo.New()
+
+  // Middlewares
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // CORS
+  e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+    AllowOrigins: []string{"*"},
+    AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+    }))
+
+  // Server
+  e.Run(standard.New(":3000"))
 }
